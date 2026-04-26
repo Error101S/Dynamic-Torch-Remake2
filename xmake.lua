f711071497c453bb940d4b6f742a15bf8801dcdb
Error101_s <keanbustos2011@gmail.com>
2026-04-26 16:30:56 -0700

    Initial upload of all files

---
+++ xmake.lua
@@ -0,0 +1,32 @@
+add_rules("mode.debug", "mode.release")
+
+add_repositories("levimc-repo https://github.com/LiteLDev/xmake-repo.git")
+
+add_requires("levilamina 1.9.8", {configs = {target_type = "client"}})
+
+add_requires("levibuildscript")
+
+if not has_config("vs_runtime") then
+    set_runtimes("MD")
+end
+
+target("DynamicLights") -- Change this to your mod name.
+    add_rules("@levibuildscript/linkrule")
+    add_rules("@levibuildscript/modpacker")
+    add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
+    add_defines("NOMINMAX", "UNICODE")
+    add_packages("levilamina")
+    set_exceptions("none") -- To avoid conflicts with /EHa.
+    set_kind("shared")
+    set_languages("c++20")
+    set_symbols("debug")
+    add_headerfiles("src/**.h")
+    add_files("src/**.cpp")
+    add_includedirs("src")
+    if is_config("target_type", "server") then
+    --  add_includedirs("src-server")
+    --  add_files("src-server/**.cpp")
+    else
+    --  add_includedirs("src-client")
+    --  add_files("src-client/**.cpp")
+    end
